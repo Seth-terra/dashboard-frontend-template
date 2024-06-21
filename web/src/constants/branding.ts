8f2175428205ff22e03ee1afc6f06123a47a9fcc
Thomas Shaw <printer.83mph@gmail.com>
2024-06-20 20:03:46 -0700

    Update branding to match TerraCompute instead of TerraHosting

--- web/src/constants/branding.ts
+++ web/src/constants/branding.ts
@@ -1,22 +1,21 @@
 // Swap out these images for your own branding!
 import BGImage from '../assets/img/bg.jpg';
-import LogoImage from '../assets/img/logo.svg';
-import HomeLogoImage from '../assets/img/logo-home.svg';
+import LogoImage from '../assets/img/logo.png';
 
 // Page title for homepage
 export const SPLASH_TITLE = `Affordable, Scalable GPUs`;
 
-export const COMPANY_NAME = 'Terra Hosting';
-export const SHORT_COMPANY_NAME = 'Terra Hosting';
+export const COMPANY_NAME = 'TerraCompute';
+export const SHORT_COMPANY_NAME = 'TerraCompute';
 
 // Switch this to `true` to use an image as your logo
 export const USE_LOGO_IMAGE = true;
-export const LOGO_TEXT = 'TerraHosting';
+export const LOGO_TEXT = 'TerraCompute';
 export const LOGO_IMAGE = LogoImage;
-export const HOME_LOGO_IMAGE = HomeLogoImage;
+export const HOME_LOGO_IMAGE = LogoImage;
 
 export const BG_IMAGE_URL = BGImage;
 
 // Update this to match your company info
 export const FOOTER_TEXT =
-  'Founded by experienced crypto miners, Terra Hosting was designed to address the specific needs and challenges of cryptocurrency mining. This strategic foresight and commitment to excellence during a challenging market phase highlight Terra Hosting’s dedication to supporting and advancing the crypto mining industry.'; //`${SHORT_COMPANY_NAME} operates a massive NVIDIA GPU cloud in the heart of the United States, providing accelerated cloud services for hobbyists, startups, and enterprises alike.`;
+  'TerraCompute.ai offers unparalleled high-performance Network Cloud GPU  services designed to supercharge your workflows while significantly saving money. Leveraging the latest NVIDIA GPUs and an ultra-fast fiber network, we provide scalable, pay-as-you-go solutions that integrate seamlessly into your existing infrastructure.';
--- web/src/constants/external.ts
+++ web/src/constants/external.ts
@@ -1,10 +1,11 @@
 // Update these!
 
-export const COMPANY_HOME_URL = 'https://terrahosting.io/';
-export const ABOUT_URL = 'https://terrahosting.io/about-us/';
-export const CUSTOM_REQUESTS_URL = 'https://terrahosting.io/contact-us/';
-export const INFRASTRUCTURE_URL = 'https://terrahosting.io/about-us/';
+export const COMPANY_HOME_URL = 'https://terracompute.ai/';
+export const ABOUT_URL = 'https://terracompute.ai/choose-terracompute-ai/';
+export const CUSTOM_REQUESTS_URL = 'https://terracompute.ai/contact/';
+export const INFRASTRUCTURE_URL =
+  'https://terracompute.ai/cloud-gpu-power-option/';
 
-export const CONTACT_EMAIL = 'contact@terrahosting.io';
-export const SALES_EMAIL = 'contact@terrahosting.io';
-export const SUPPORT_EMAIL = 'contact@terrahosting.io';
+export const CONTACT_EMAIL = 'info@terracompute.ai';
+export const SALES_EMAIL = 'info@terracompute.ai';
+export const SUPPORT_EMAIL = 'info@terracompute.ai';
