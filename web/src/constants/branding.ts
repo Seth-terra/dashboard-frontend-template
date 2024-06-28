ae89031ccb369f1a678b3636def3c2443b1506b4
Seth-terra <seth@terrahosting.io>
2024-06-28 10:38:08 -0400

    Update branding.ts
    added a space in between Terra and Compute

--- web/src/constants/branding.ts
+++ web/src/constants/branding.ts
@@ -5,12 +5,12 @@ import LogoImage from '../assets/img/logo.png';
 // Page title for homepage
 export const SPLASH_TITLE = `Affordable, Scalable GPUs`;
 
-export const COMPANY_NAME = 'TerraCompute';
-export const SHORT_COMPANY_NAME = 'TerraCompute';
+export const COMPANY_NAME = 'Terra Compute';
+export const SHORT_COMPANY_NAME = 'Terra Compute';
 
 // Switch this to `true` to use an image as your logo
 export const USE_LOGO_IMAGE = true;
-export const LOGO_TEXT = 'TerraCompute';
+export const LOGO_TEXT = 'Terra Compute';
 export const LOGO_IMAGE = LogoImage;
 export const HOME_LOGO_IMAGE = LogoImage;
 
@@ -18,4 +18,4 @@ export const BG_IMAGE_URL = BGImage;
 
 // Update this to match your company info
 export const FOOTER_TEXT =
-  'TerraCompute.ai offers unparalleled high-performance Network Cloud GPU  services designed to supercharge your workflows while significantly saving money. Leveraging the latest NVIDIA GPUs and an ultra-fast fiber network, we provide scalable, pay-as-you-go solutions that integrate seamlessly into your existing infrastructure.';
+  'TerraCompute.ai offers unparalleled high-performance Network Cloud GPU services designed to supercharge your workflows while significantly saving money. Leveraging the latest NVIDIA GPUs and an ultra-fast fiber network, we provide scalable, pay-as-you-go solutions that integrate seamlessly into your existing infrastructure.';
